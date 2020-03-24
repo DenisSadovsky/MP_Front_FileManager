e59f3e42580d5669fdbb834b0c420f6e39d8d54c
Dzianis Sadouski <Dzianis_Sadouski@epam.com>
2020-03-24 20:14:01 +0300

    Add project files.

---
+++ FileManager_MP/Controllers/AccountController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FileManager_MP.Models;
+
+namespace FileManager_MP.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+
+        // GET: Account
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        void ConnectionString()
+        {
+            con.ConnectionString = @"data source=EPBYMINW0728; database=FileManager; Trusted_Connection=True;";
+
+        }
+
+        [HttpPost]
+        public ActionResult Login(Account acc)
+        {
+            ConnectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select IdRole from tbl_Accounts where UserName='" + acc.Name + "' and Password='" +
+                              acc.Password + "'";
+            //dr = com.ExecuteReader();
+            var userRole = com.ExecuteScalar();
+            if (userRole != null)
+            {
+                if (userRole.ToString() == "1")
+                {
+                    //Admin
+                    con.Close();
+                    return View("Create");
+                }
+
+                con.Close();
+                return View("Create");
+                //vonychka
+
+            }
+
+            con.Close();
+            return View("Error");
+        }
+
+        [HttpPost]
+        public ActionResult Create(FileDirectory acc)
+        {
+            acc.GetDirectoryFiles(acc.Path);
+            return View(acc);
+        }
+
+        [HttpPost]
+        public void Create(string acc)
+        {
+            var d = new FileDirectory();
+            d.CreateDirectory(acc);
+            //return View(acc);
+        }
+
+    }
+
+   
+}
---
+++ FileManager_MP/Models/FileDirectory.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace FileManager_MP.Models
+{
+    public class FileDirectory
+    {
+        public string Path { get; set; }
+
+        public string[] Files { get; set; }
+
+        public string[] Directories { get; set; }
+
+        public string Message { get; set; }
+
+
+        public void GetDirectoryFiles(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                Directories = Directory.GetDirectories(path);
+                
+                Files = Directory.GetFiles(path);
+            }
+        }
+
+        public void DeleteFile()
+        {
+            Directory.Delete(Path, true);
+        }
+
+        public void CreateFile(string path)
+        {
+            try
+            {
+                FileStream fs = File.Create(path);
+                Message = "File created successfully";
+            }
+            catch (Exception ex)
+            {
+                Message =  "File not created";
+            }
+
+        }
+        public void CreateDirectory(string path)
+        {
+
+        }
+    }
+}
